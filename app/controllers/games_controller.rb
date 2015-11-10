16bc03d6ac205b73d6301c7bf74e750ec3a0bf73
Jeff Gerlach <jeffgerlach06@gmail.com>
2015-11-09 23:46:09 -0700

    Flesh out Games controller actions, implement new game form.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,30 @@
 class GamesController < ApplicationController
   def new
+    @game = Game.new
   end
 
   def create
+    @game = current_user.simple_wods.create(simple_wod_params)
+    if @game.valid?
+      redirect_to game_path(@game)
+    else
+      flash[:alert] = "Error creating game!"
+      render :new, :status => :unprocessable_entity
+    end
   end
 
   def show
+  rescue ActiveRecord::RecordNotFound 
+      render :text => "404 Error - Game Not Found", :status => :not_found
+  end
+
+private
+  helper_method :current_game
+  def current_game
+    @current_game ||= Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:game_name)  
   end
 end
