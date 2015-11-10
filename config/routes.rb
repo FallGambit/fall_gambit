234635047ee674690d4a31a74fc23bda4c517048
Jeff Gerlach <jeffgerlach06@gmail.com>
2015-11-09 23:29:00 -0700

    Update routes for games controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 FallGambit::Application.routes.draw do
-  get "games/new"
-  get "games/create"
-  get "games/show"
+  
+  resources :games, :only => [:new,, :create, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
