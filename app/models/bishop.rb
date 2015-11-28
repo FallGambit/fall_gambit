e95db5a3b284d3efdb9760b78402ca2bff3ebaa7
Jeff Gerlach <jeffgerlach06@gmail.com>
2015-11-27 22:56:48 -0700

    Add valid move method to bishop class

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -2,4 +2,9 @@ class Bishop < Piece
   def is_obstructed?
     # diag only
   end
+
+  def valid_move?(x, y)
+    # can only move diagonally
+    (x_position - x).abs == (y_position - y).abs
+  end
 end
