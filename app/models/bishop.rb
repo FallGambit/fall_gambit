ddf3e21d2b8098d3058e272bb0ffda752ac4efb4
Jeff Gerlach <jeffgerlach06@gmail.com>
2015-11-27 23:33:40 -0700

    Add is_obstructed call to valid_move method. Add tests for both valid and invalid bishop moves.

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,10 +1,7 @@
 class Bishop < Piece
-  def is_obstructed?
-    # diag only
-  end
-
   def valid_move?(x, y)
     # can only move diagonally
+    is_obstructed?(x, y)
     (x_position - x).abs == (y_position - y).abs
   end
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe "moving pieces" do
+    context "valid moves" do
+      it "validates moves for bishops" do
+        bishop_game = create(:game)
+        lpawn = bishop_game.pawns.where(x_position: 1, y_position: 1).first
+        rpawn = bishop_game.pawns.where(x_position: 1, y_position: 1).first
+        bishop = bishop_game.bishops.where(x_position: 2, y_position: 0).first
+        expect(bishop.valid_move?(3, 1)).to be true
+        expect(bishop.valid_move?(1, 1)).to be true
+        lpawn.update_attributes(y_position: 2)
+        rpawn.update_attributes(y_position: 2)
+        # try moving around a diamond shape
+        expect(bishop.valid_move?(4, 2)).to be true
+        # this is just updating attributes, so pieces can move through others
+        bishop.update_attributes(x_position: 4, y_position: 2)
+        expect(bishop.valid_move?(2, 4)).to be true
+        bishop.update_attributes(x_position: 2, y_position: 4)
+        expect(bishop.valid_move?(0, 6)).to be true
+        expect(bishop.valid_move?(0, 2)).to be true
+        bishop.update_attributes(x_position: 0, y_position: 2)
+        expect(bishop.valid_move?(2, 0)).to be true
+        bishop.update_attributes(x_position: 2, y_position: 0)
+      end
+    end
+    context "invalid moves" do
+      it "invalidates moves for bishops" do
+        bishop_game = create(:game)
+        bishop = bishop_game.bishops.where(x_position: 2, y_position: 0).first
+        expect(bishop.valid_move?(1, 0)).to be false
+        expect(bishop.valid_move?(3, 0)).to be false
+        expect(bishop.valid_move?(2, 1)).to be false
+        bishop.update_attributes(y_position: 4)
+        expect(bishop.valid_move?(2, 2)).to be false
+        expect(bishop.valid_move?(4, 4)).to be false
+        expect(bishop.valid_move?(2, 6)).to be false
+      end
+    end
+  end
+  end
