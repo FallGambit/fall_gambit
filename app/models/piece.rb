0dce415dd2292b52fe2459ead9912f63d09a3dee
amarkpark <nospam@amarkpark.com>
2015-11-29 00:58:02 +0000

    ran rubocop -a

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -26,7 +26,7 @@ class Piece < ActiveRecord::Base
   def is_obstructed?(x, y)
     dest_x = x
     dest_y = y
-    state_x = x_position 
+    state_x = x_position
     state_y = y_position
     delta_x = dest_x - state_x
     delta_y = dest_y - state_y
@@ -34,66 +34,66 @@ class Piece < ActiveRecord::Base
 
     if delta_y == 0 && delta_x > 0 && delta_x.abs > 1
       # horizontal move where start is < destination and distance > 1
-      return game.pieces.where("y_position = ? AND x_position BETWEEN ? AND ?", state_y, (state_x+1), (dest_x-1)).any?
+      return game.pieces.where("y_position = ? AND x_position BETWEEN ? AND ?", state_y, (state_x + 1), (dest_x - 1)).any?
     elsif delta_y == 0 && delta_x < 0 && delta_x.abs > 1
       # horizontal move where start is > destination and distance > 1
-      return game.pieces.where("y_position = ? AND x_position BETWEEN ? AND ?", state_y, (dest_x+1), (state_x-1)).any?
+      return game.pieces.where("y_position = ? AND x_position BETWEEN ? AND ?", state_y, (dest_x + 1), (state_x - 1)).any?
     elsif delta_y == 0 && delta_x.abs <= 1
       # horizontal move to next square or delta 0
       return false
     elsif delta_x == 0 && delta_y > 0 && delta_y.abs > 1
       # vertical move where start is < destination and distance > 1
-      return game.pieces.where("x_position = ? AND y_position BETWEEN ? AND ?", state_x, (state_y+1), (dest_y-1)).any?
+      return game.pieces.where("x_position = ? AND y_position BETWEEN ? AND ?", state_x, (state_y + 1), (dest_y - 1)).any?
     elsif delta_x == 0 && delta_y < 0 && delta_y.abs > 1
       # vertical move where start is > destination and distance > 1
-      return game.pieces.where("y_position = ? AND x_position BETWEEN ? AND ?", state_x, (dest_y+1), (state_y-1)).any?
+      return game.pieces.where("y_position = ? AND x_position BETWEEN ? AND ?", state_x, (dest_y + 1), (state_y - 1)).any?
     elsif delta_x == 0 && delta_y.abs <= 1
       # vertical move to next square or delta 0
       return false
     elsif delta_x == delta_y && delta_x > 0
       # SE move, positive X, positive Y diagonal
       steps = delta_x - 1
-      while steps != 0 do
+      while steps != 0
         if game.pieces.where("x_position = ? AND y_position = ?", (state_x + steps), (state_y + steps)).any?
           return true
         end
-        steps -= 1 
+        steps -= 1
       end
       return false
     elsif delta_x == delta_y && delta_x < 0
       # NW move, negative X negative Y diagonal
       steps = delta_x.abs - 1
-      while steps != 0 do
+      while steps != 0
         if game.pieces.where("x_position = ? AND y_position = ?", (state_x - steps), (state_y - steps)).any?
           return true
         end
-        steps -= 1 
+        steps -= 1
       end
       return false
     elsif delta_x > 0 && delta_y < 0 && delta_x == delta_y.abs
       # NE move, positive X, negative Y diagonal
       steps = delta_x - 1
-      while steps != 0 do
+      while steps != 0
         if game.pieces.where("x_position = ? AND y_position = ?", (state_x + steps), (state_y - steps)).any?
           return true
         end
-        steps -= 1 
+        steps -= 1
       end
       return false
     elsif delta_x < 0 && delta_y > 0 && delta_x.abs == delta_y
       # SW move, negative X, positive Y diagonal
       steps = delta_y - 1
-      while steps != 0 do
+      while steps != 0
         if game.pieces.where("x_position = ? AND y_position = ?", (state_x - steps), (state_y + steps)).any?
           return true
         end
-        steps -= 1 
+        steps -= 1
       end
       return false
     elsif delta_x != 0 && delta_y != 0 && delta_x.abs != delta_y.abs
       # this handles invalid input or invalid moves.
       return "Invalid input or invalid move."
-      # Should this raise an exception? rescue an exception? 
+      # Should this raise an exception? rescue an exception?
       # should it return true or false?
       # if an error, is it a Standard Error? An Argument Error?
     end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -13,72 +13,71 @@ RSpec.describe Piece, type: :model do
         color: true,
         game_id: @game.id,
         captured: false
-        ) 
+      )
       @white_knight = Knight.create(
         x_position: 0,
         y_position: 1,
         color: true,
         game_id: @game.id,
         captured: false
-        ) 
+      )
       @white_pawn = Pawn.create(
         x_position: 1,
         y_position: 1,
         color: true,
         game_id: @game.id,
         captured: false
-        ) 
+      )
       @white_bishop = Bishop.create(
         x_position: 4,
         y_position: 2,
-        color: true, 
+        color: true,
         game_id: @game.id,
         captured: false
-        )  
+      )
       @black_pawn = Pawn.create(
         x_position: 4,
         y_position: 6,
         color: false,
         game_id: @game.id,
         captured: false
-        ) 
+      )
     end
 
     it 'evaluates as false when diagonal path is clear' do
-      expect(@white_queen.is_obstructed?(2,4)).to be_falsey
+      expect(@white_queen.is_obstructed?(2, 4)).to be_falsey
     end
 
     it 'evaluates as false when horizontal axis path is clear' do
-      expect(@white_queen.is_obstructed?(3,2)).to be_falsey
+      expect(@white_queen.is_obstructed?(3, 2)).to be_falsey
     end
 
     it 'evaluates as false when vertical axis path is clear' do
-      expect(@white_queen.is_obstructed?(0,5)).to be_falsey
+      expect(@white_queen.is_obstructed?(0, 5)).to be_falsey
     end
 
     it 'evaluates as true when there is a block in horizontal axis' do
-      expect(@white_queen.is_obstructed?(5,2)).to be_truthy
+      expect(@white_queen.is_obstructed?(5, 2)).to be_truthy
     end
 
     it 'evaluates as true when there is a block in vertical axis' do
-      expect(@white_queen.is_obstructed?(0,0)).to be_truthy
+      expect(@white_queen.is_obstructed?(0, 0)).to be_truthy
     end
 
     it 'evaluates as true when there is a block in the diagonal' do
-      expect(@white_queen.is_obstructed?(2,0)).to be_truthy
+      expect(@white_queen.is_obstructed?(2, 0)).to be_truthy
     end
 
-    it 'will raise an Error Message with invalid input' do 
-      expect(@white_queen.is_obstructed?(3,4)).to raise_error # add "invalid input"
+    it 'will raise an Error Message with invalid input' do
+      expect(@white_queen.is_obstructed?(3, 4)).to raise_error # add "invalid input"
     end
 
-    it 'evaluates as false when PATH is clear but DESTINATION contains a piece' do
-      expect(@white_queen.is_obstructed?(1,1)).to be_falsey
+    it 'evaluates false with clear path if destination contains piece' do
+      expect(@white_queen.is_obstructed?(1, 1)).to be_falsey
     end
 
-    # it 'evaluates as true when path is clear but destination contains same-color' do
+    # it 'evaluates true w/ clear path, destination piece same-color' do
     #   expect(@white_queen.is_obstructed?(4,2)).to be_truthy
     # end # this will probably be covered by is_valid
-
   end
 end
