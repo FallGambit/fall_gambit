fed59bf9df3d1f494fb7578dbe65c9c15e1efc9c
amarkpark <nospam@amarkpark.com>
2015-11-30 00:32:08 +0000

    Refactored with self.game as suggested by Jeff. ~AMP

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -36,23 +36,22 @@ class Piece < ActiveRecord::Base
     state_y = y_position
     delta_x = dest_x - state_x
     delta_y = dest_y - state_y
-    game = Game.find(game_id)
 
     if delta_y == 0 && delta_x > 0 && delta_x.abs > 1
       # horizontal move where start is < destination and distance > 1
-      return game.pieces.where("y_position = ? AND x_position BETWEEN ? AND ?", state_y, (state_x + 1), (dest_x - 1)).any?
+      return self.game.pieces.where("y_position = ? AND x_position BETWEEN ? AND ?", state_y, (state_x + 1), (dest_x - 1)).any?
     elsif delta_y == 0 && delta_x < 0 && delta_x.abs > 1
       # horizontal move where start is > destination and distance > 1
-      return game.pieces.where("y_position = ? AND x_position BETWEEN ? AND ?", state_y, (dest_x + 1), (state_x - 1)).any?
+      return self.game.pieces.where("y_position = ? AND x_position BETWEEN ? AND ?", state_y, (dest_x + 1), (state_x - 1)).any?
     elsif delta_y == 0 && delta_x.abs <= 1
       # horizontal move to next square or delta 0
       return false
     elsif delta_x == 0 && delta_y > 0 && delta_y.abs > 1
       # vertical move where start is < destination and distance > 1
-      return game.pieces.where("x_position = ? AND y_position BETWEEN ? AND ?", state_x, (state_y + 1), (dest_y - 1)).any?
+      return self.game.pieces.where("x_position = ? AND y_position BETWEEN ? AND ?", state_x, (state_y + 1), (dest_y - 1)).any?
     elsif delta_x == 0 && delta_y < 0 && delta_y.abs > 1
       # vertical move where start is > destination and distance > 1
-      return game.pieces.where("x_position = ? AND y_position BETWEEN ? AND ?", state_x, (dest_y + 1), (state_y - 1)).any?
+      return self.game.pieces.where("x_position = ? AND y_position BETWEEN ? AND ?", state_x, (dest_y + 1), (state_y - 1)).any?
     elsif delta_x == 0 && delta_y.abs <= 1
       # vertical move to next square or delta 0
       return false
@@ -60,7 +59,7 @@ class Piece < ActiveRecord::Base
       # SE move, positive X, positive Y diagonal
       steps = delta_x - 1
       while steps != 0
-        if game.pieces.where("x_position = ? AND y_position = ?", (state_x + steps), (state_y + steps)).any?
+        if self.game.pieces.where("x_position = ? AND y_position = ?", (state_x + steps), (state_y + steps)).any?
           return true
         end
         steps -= 1
@@ -70,7 +69,7 @@ class Piece < ActiveRecord::Base
       # NW move, negative X negative Y diagonal
       steps = delta_x.abs - 1
       while steps != 0
-        if game.pieces.where("x_position = ? AND y_position = ?", (state_x - steps), (state_y - steps)).any?
+        if self.game.pieces.where("x_position = ? AND y_position = ?", (state_x - steps), (state_y - steps)).any?
           return true
         end
         steps -= 1
@@ -80,7 +79,7 @@ class Piece < ActiveRecord::Base
       # NE move, positive X, negative Y diagonal
       steps = delta_x - 1
       while steps != 0
-        if game.pieces.where("x_position = ? AND y_position = ?", (state_x + steps), (state_y - steps)).any?
+        if self.game.pieces.where("x_position = ? AND y_position = ?", (state_x + steps), (state_y - steps)).any?
           return true
         end
         steps -= 1
@@ -90,7 +89,7 @@ class Piece < ActiveRecord::Base
       # SW move, negative X, positive Y diagonal
       steps = delta_y - 1
       while steps != 0
-        if game.pieces.where("x_position = ? AND y_position = ?", (state_x - steps), (state_y + steps)).any?
+        if self.game.pieces.where("x_position = ? AND y_position = ?", (state_x - steps), (state_y + steps)).any?
           return true
         end
         steps -= 1
@@ -98,13 +97,7 @@ class Piece < ActiveRecord::Base
       return false
     elsif delta_x != 0 && delta_y != 0 && delta_x.abs != delta_y.abs
       # this handles invalid input or invalid moves.
-      # raise RuntimeError, "Invalid input or invalid move."
-      # raise
-      # self.errors.invalid?
-      # self.errors.add(:is_obstructed, "is invalid")
-      # self.generate_message(attribute, type = :invalid, options = {})
-      # self.errors.default_error_messages()
-      # self.valid? = false
+      # raise "Invalid input or invalid move."
       return "Invalid input or invalid move."
     end
   end
