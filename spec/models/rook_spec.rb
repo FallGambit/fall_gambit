643beacf9f6edb67e1f62c3a61d257ccb9e84b2f
Nicola Guidi <nicola.guidi@me.com>
2015-12-03 04:10:39 +0100

    Change tests to make sure correct cases are checked

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Rook, type: :model do
   it "should be true when moving 1 space up" do
     board = create(:game)
     board.pieces.delete_all
-    rook = Rook.new(x_position: 1, y_position: 0)
+    rook = Rook.create(x_position: 1, y_position: 0)
+    board.reload
     actual = rook.valid_move?(1, 1)
     expect(actual).to be(true)
   end
@@ -12,7 +13,8 @@ RSpec.describe Rook, type: :model do
   it "should be true when moving 2 spaces right" do
     board = create(:game)
     board.pieces.delete_all
-    rook = Rook.new(x_position: 1, y_position: 0)
+    rook = Rook.create(x_position: 1, y_position: 0, game_id: board.id)
+    board.reload
     actual = rook.valid_move?(3, 0)
     expect(actual).to be(true)
   end
@@ -20,7 +22,8 @@ RSpec.describe Rook, type: :model do
   it "should be true when moving 3 spaces left" do
     board = create(:game)
     board.pieces.delete_all
-    rook = Rook.new(x_position: 4, y_position: 3)
+    rook = Rook.create(x_position: 4, y_position: 3, game_id: board.id)
+    board.reload
     actual = rook.valid_move?(1, 3)
     expect(actual).to be(true)
   end
@@ -28,7 +31,8 @@ RSpec.describe Rook, type: :model do
   it "should be true when moving 4 spaces down" do
     board = create(:game)
     board.pieces.delete_all
-    rook = Rook.new(x_position: 4, y_position: 4)
+    rook = Rook.create(x_position: 4, y_position: 4, game_id: board.id)
+    board.reload
     actual = rook.valid_move?(4, 0)
     expect(actual).to be(true)
   end
@@ -36,7 +40,8 @@ RSpec.describe Rook, type: :model do
   it "should be false when moving 1 space diagonal" do
     board = create(:game)
     board.pieces.delete_all
-    rook = Rook.new(x_position: 1, y_position: 0)
+    rook = Rook.create(x_position: 1, y_position: 0, game_id: board.id)
+    board.reload
     actual = rook.valid_move?(2, 1)
     expect(actual).to be(false)
   end
@@ -44,7 +49,8 @@ RSpec.describe Rook, type: :model do
   it "should be false when moving 3 spaces diagonally" do
     board = create(:game)
     board.pieces.delete_all
-    rook = Rook.new(x_position: 1, y_position: 0)
+    rook = Rook.create(x_position: 1, y_position: 0, game_id: board.id)
+    board.reload
     actual = rook.valid_move?(4, 3)
     expect(actual).to be(false)
   end
