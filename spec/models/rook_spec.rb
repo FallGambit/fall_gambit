99fc9d644a548f50c7b16fd9c9a257654ff070b3
Nicola Guidi <nicola.guidi@me.com>
2015-12-03 04:05:21 +0100

    Use delete_all method to make sure the board is wiped clean

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Rook, type: :model do
   it "should be true when moving 1 space up" do
     board = create(:game)
-    board.pieces.delete
+    board.pieces.delete_all
     rook = Rook.new(x_position: 1, y_position: 0)
     actual = rook.valid_move?(1, 1)
     expect(actual).to be(true)
@@ -11,7 +11,7 @@ RSpec.describe Rook, type: :model do
 
   it "should be true when moving 2 spaces right" do
     board = create(:game)
-    board.pieces.delete
+    board.pieces.delete_all
     rook = Rook.new(x_position: 1, y_position: 0)
     actual = rook.valid_move?(3, 0)
     expect(actual).to be(true)
@@ -19,7 +19,7 @@ RSpec.describe Rook, type: :model do
 
   it "should be true when moving 3 spaces left" do
     board = create(:game)
-    board.pieces.delete
+    board.pieces.delete_all
     rook = Rook.new(x_position: 4, y_position: 3)
     actual = rook.valid_move?(1, 3)
     expect(actual).to be(true)
@@ -27,7 +27,7 @@ RSpec.describe Rook, type: :model do
 
   it "should be true when moving 4 spaces down" do
     board = create(:game)
-    board.pieces.delete
+    board.pieces.delete_all
     rook = Rook.new(x_position: 4, y_position: 4)
     actual = rook.valid_move?(4, 0)
     expect(actual).to be(true)
@@ -35,7 +35,7 @@ RSpec.describe Rook, type: :model do
 
   it "should be false when moving 1 space diagonal" do
     board = create(:game)
-    board.pieces.delete
+    board.pieces.delete_all
     rook = Rook.new(x_position: 1, y_position: 0)
     actual = rook.valid_move?(2, 1)
     expect(actual).to be(false)
@@ -43,7 +43,7 @@ RSpec.describe Rook, type: :model do
 
   it "should be false when moving 3 spaces diagonally" do
     board = create(:game)
-    board.pieces.delete
+    board.pieces.delete_all
     rook = Rook.new(x_position: 1, y_position: 0)
     actual = rook.valid_move?(4, 3)
     expect(actual).to be(false)
