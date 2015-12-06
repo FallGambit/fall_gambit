d8eda1a37889c820f8c3e67cfb3d514d798c74ba
Jeff Gerlach <jeffgerlach06@gmail.com>
2015-12-06 14:55:17 -0700

    Add has_moved default of false to pieces

--- db/migrate/20151206214122_add_has_moved_to_pieces.rb
+++ db/migrate/20151206214122_add_has_moved_to_pieces.rb
@@ -1,6 +1,6 @@
 class AddHasMovedToPieces < ActiveRecord::Migration
   def self.up
-    add_column :pieces, :has_moved, :boolean
+    add_column :pieces, :has_moved, :boolean, default: false
   end
   def self.down
     remove_column :pieces, :has_moved
