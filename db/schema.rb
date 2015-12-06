9816f31afba15f542d818c7c350057838367e22c
Jeff Gerlach <jeffgerlach06@gmail.com>
2015-12-06 14:44:24 -0700

    Add field to track if a piece has moved for castling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127190204) do
+ActiveRecord::Schema.define(version: 20151206214122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20151127190204) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image_name"
+    t.boolean  "has_moved"
   end
 
   create_table "users", force: true do |t|
