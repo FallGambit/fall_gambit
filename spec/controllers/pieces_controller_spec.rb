19f3f9298c2fb694805a372c8e076ff551c77935
acodeinprogress <sjsieg830@gmail.com>
2015-12-07 19:27:16 -0700

    Added tests for movement and update in pieces controller

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -1,25 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe PiecesController, type: :controller do
-  before :all do
+  before :each do
     @black_user = FactoryGirl.create(:user)
     @white_user = FactoryGirl.create(:user)
     sign_in @white_user
     sign_in @black_user
-    @current_game = FactoryGirl.create(:game, :white_user => @white_user.id, :black_user => @black_user.id)
+    @current_game = FactoryGirl.create(:game)
   end
 
-  describe "pawn should move" do
-    it "will move" do
-      controller = PiecesController.new
+  describe "movement" do
+    it "will move 2 spaces up" do
       @piece = Piece.where(x_position: 6, y_position: 6).first
-      put :update
-      controller.instance_eval{ show_piece_td(6, 5) } 
-      expect{ @piece.x_y_coords }.to eq ([6, 5])
+      put :update, { id: @piece.id, x: 6, y: 4 }
+      expected_y_position = 4
+      @piece.reload
+      expect(@piece.y_position).to be(expected_y_position)
+    end
+
+    it "will move 2 spaces left" do
+      @piece = Piece.where(x_position: 6, y_position: 6).first
+      put :update, { id: @piece.id, x: 4, y: 6 }
+      expected_x_position = 4
+      @piece.reload
+      expect(@piece.x_position).to be(expected_x_position)
     end
   end
-  after :all do
-    @current_game.delete
-    Piece.delete_all
+
+  describe "#update" do
+    it "will redirect to games page" do
+      @piece = Piece.where(x_position: 6, y_position: 6).first
+      put :update, { id: @piece.id, x: 4, y: 6 }
+      expect(response).to redirect_to :controller => :games, :action => :show, :id => @piece.game
+    end
   end
 end
