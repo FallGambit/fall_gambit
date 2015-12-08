337c6f4fd2f68b631abf81df8b446ee450d68e85
Nicola Guidi <nicola.guidi@me.com>
2015-12-08 20:47:32 +0100

    Add tests for Pawn#valid_move?

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,8 +1,26 @@
 class Pawn < Piece
-  def first_move
-    # can move two spaces forward but only on first move
-  end
+  def valid_move?(x, y)
+    is_obstructed?(x, y)
+    if white_moving?
 
-  def move
+    else
+      # black_moving
+    end
+  end
+  # def first_move
+  #   # can move two spaces forward but only on first move
+  # end
+  #
+  # def move
+  # end
+  def white_moving?
+    color
   end
 end
+
+
+# First move, allowed to move 2
+# All moves allowed to move 1
+# Can’t capture vertically
+# Up one, horizontal 1 to capture
+# Don’t worry about en passant - we can worry about that later
