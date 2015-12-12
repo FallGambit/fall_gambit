f28c3262eb78bffd43807aff65d97697aad68545
amarkpark <nospam@amarkpark.com>
2015-12-12 21:02:28 +0000

    Started framing tests for Queen valid_move?. ~AMP

--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -1,5 +1,110 @@
 require 'rails_helper'
 
 RSpec.describe Queen, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#valid_move?" do
+    context "diagonal moves" do
+      it "passes +X +Y no obstruction no end piece" do
+
+      end
+      it "passes +X -Y no obstruction no end piece" do
+
+      end
+      it "passes -X -Y no obstruction no end piece" do
+
+      end
+      it "passes -X +Y no obstruction no end piece" do
+
+      end
+      it "passes +X +Y no obstr opposite color end piece" do
+
+      end
+      it "passes +X -Y no obstr opposite color end piece" do
+
+      end
+      it "passes -X -Y no obstr opposite color end piece" do
+
+      end
+      it "passes -X +Y no obstr opposite color end piece" do
+
+      end
+      it "fails +X +Y no obstr same color end piece" do
+
+      end
+      it "fails +X -Y no obstr same color end piece" do
+
+      end
+      it "fails -X -Y no obstr same color end piece" do
+
+      end
+      it "fails -X +Y no obstr same color end piece" do
+
+      end
+      it "fails +X +Y with obstruction" do
+
+      end
+      it "fails +X -Y with obstruction" do
+
+      end
+      it "fails -X -Y with obstruction" do
+
+      end
+      it "fails -X +Y with obstruction" do
+
+      end
+    end
+
+    context "horizontal moves" do
+      it "passes +Y no obstruction no end piece" do
+
+      end
+      it "passes -Y no obstruction no end piece" do
+
+      end
+      it "passes +Y no obstr opposite color end piece" do
+
+      end
+      it "passes -Y no obstr opposite color end piece" do
+
+      end
+      it "fails +Y no obstr same color end piece" do
+
+      end
+      it "fails -Y no obstr same color end piece" do
+
+      end
+      it "fails +Y with obstruction" do
+
+      end
+      it "fails -Y with obstruction" do
+
+      end
+    end
+
+    context "vertical moves" do
+      it "passes +X no obstruction no end piece" do
+
+      end
+      it "passes -X no obstruction no end piece" do
+
+      end
+      it "passes +X no obstr opposite color end piece" do
+
+      end
+      it "passes -X no obstr opposite color end piece" do
+
+      end
+      it "fails +X no obstr same color end piece" do
+
+      end
+      it "fails -X no obstr same color end piece" do
+
+      end
+      it "fails +X with obstruction" do
+
+      end
+      it "fails -X with obstruction" do
+
+      end
+    end
+  end
 end
