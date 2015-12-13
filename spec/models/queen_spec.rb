8a5a2b45812575d05d7205345ec819a9aed808b1
amarkpark <nospam@amarkpark.com>
2015-12-13 22:32:48 +0000

    YAY! The tests are FINALLY failing in the right way! ~AMP

--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -6,8 +6,6 @@ RSpec.describe Queen, type: :model do
       @board = create(:game)
       @board.pieces.delete_all
       @board.reload
-      # @white_user = User.find(@board.white_user_id)
-      # @black_user = User.find(@board.black_user_id)
     end
     context "no obstruction, no end piece, all pass:" do
       before :all do
@@ -46,54 +44,69 @@ RSpec.describe Queen, type: :model do
     end
     context "no obstruction, opposite color end piece, all pass:" do
       before :all do
-        @white_queen.update(y_position: 0)
+        @board.pieces.delete_all
         @black_queen = Queen.create(
           x_position: 3,
           y_position: 2,
           color: false,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.black_user_id
+          )
+        @white_queen = Queen.create(
+          x_position: 3,
+          y_position: 0,
+          color: true,
+          game_id: @board.id,
+          user_id: @board.white_user_id
           )
         @white_rook = Rook.create(
           x_position: 1,
           y_position: 0,
           color: true,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.white_user_id
           )
         @white_pawn1 = Pawn.create(
           x_position: 1,
           y_position: 2,
           color: true,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.white_user_id
           )
         @white_knight1 = Knight.create(
           x_position: 1,
           y_position: 4,
           color: true,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.white_user_id
           )
         @white_bishop1 = Bishop.create(
           x_position: 3,
           y_position: 4,
           color: true,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.white_user_id
           )
         @white_pawn2 = Pawn.create(
           x_position: 5,
           y_position: 4,
           color: true,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.white_user_id
           )
         @white_knight2 = Knight.create(
           x_position: 5,
           y_position: 2,
           color: true,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.white_user_id
           )
         @white_bishop2 = Bishop.create(
           x_position: 5,
           y_position: 0,
           color: true,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.white_user_id
           )
       end
       it "diagonal +X +Y" do
@@ -123,14 +136,69 @@ RSpec.describe Queen, type: :model do
     end
     context "no obstruction, same color end piece, all fail:" do
       before :all do
-        byebug
-        @black_queen.update(x_position: 5, y_position: 5)
-        @white_queen.update(y_position: 2)
+        @board.pieces.delete_all
+        @white_queen = Queen.create(
+          x_position: 3,
+          y_position: 2,
+          color: true,
+          game_id: @board.id,
+          user_id: @board.white_user_id
+          )
         @white_king = King.create(
           x_position: 3,
           y_position: 0,
           color: true,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.white_user_id
+          )
+        @white_rook = Rook.create(
+          x_position: 1,
+          y_position: 0,
+          color: true,
+          game_id: @board.id,
+          user_id: @board.white_user_id
+          )
+        @white_pawn1 = Pawn.create(
+          x_position: 1,
+          y_position: 2,
+          color: true,
+          game_id: @board.id,
+          user_id: @board.white_user_id
+          )
+        @white_knight1 = Knight.create(
+          x_position: 1,
+          y_position: 4,
+          color: true,
+          game_id: @board.id,
+          user_id: @board.white_user_id
+          )
+        @white_bishop1 = Bishop.create(
+          x_position: 3,
+          y_position: 4,
+          color: true,
+          game_id: @board.id,
+          user_id: @board.white_user_id
+          )
+        @white_pawn2 = Pawn.create(
+          x_position: 5,
+          y_position: 4,
+          color: true,
+          game_id: @board.id,
+          user_id: @board.white_user_id
+          )
+        @white_knight2 = Knight.create(
+          x_position: 5,
+          y_position: 2,
+          color: true,
+          game_id: @board.id,
+          user_id: @board.white_user_id
+          )
+        @white_bishop2 = Bishop.create(
+          x_position: 5,
+          y_position: 0,
+          color: true,
+          game_id: @board.id,
+          user_id: @board.white_user_id
           )
       end
       it "diagonal +X +Y" do
@@ -160,47 +228,69 @@ RSpec.describe Queen, type: :model do
     end
     context "with obstruction, all fail:" do
       before :all do
+        @board.pieces.delete_all
+        @black_queen = Queen.create(
+          x_position: 5,
+          y_position: 5,
+          color: false,
+          game_id: @board.id,
+          user_id: @board.black_user_id
+          )
         @black_pawn1 = Pawn.create(
           x_position: 4,
           y_position: 6,
           color: false,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.black_user_id
           )
         @black_pawn2 = Pawn.create(
           x_position: 5,
           y_position: 6,
           color: false,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.black_user_id
           )
         @black_pawn3 = Pawn.create(
           x_position: 6,
           y_position: 6,
           color: false,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.black_user_id
           )
         @black_rook = Rook.create(
           x_position: 6,
           y_position: 5,
           color: false,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.black_user_id
           )
         @black_bishop = Bishop.create(
           x_position: 4,
           y_position: 5,
           color: false,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.black_user_id
           )
-        @white_pawn3 = Pawn.create(
+        @white_pawn1 = Pawn.create(
+          x_position: 5,
+          y_position: 4,
+          color: true,
+          game_id: @board.id,
+          user_id: @board.white_user_id
+          )
+        @white_pawn2 = Pawn.create(
           x_position: 4,
           y_position: 4,
           color: false,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.white_user_id
           )
-        @white_pawn4 = Pawn.create(
+        @white_pawn3 = Pawn.create(
           x_position: 6,
           y_position: 4,
           color: false,
-          game_id: @board.id
+          game_id: @board.id,
+          user_id: @board.white_user_id
           )
       end
       it "diagonal +X +Y" do
@@ -229,10 +319,27 @@ RSpec.describe Queen, type: :model do
       end
     end
     context "invalid coordinates, all fail:" do
+      before :all do
+        @board.pieces.delete_all
+      end
       it "tries to move off the board" do
+        @black_queen = Queen.create(
+          x_position: 5,
+          y_position: 5,
+          color: false,
+          game_id: @board.id,
+          user_id: @board.black_user_id
+          )
         expect(@black_queen.valid_move?(8, 2)).to eq false
       end
       it "tries to make invalid move" do
+        @white_queen = Queen.create(
+          x_position: 3,
+          y_position: 2,
+          color: true,
+          game_id: @board.id,
+          user_id: @board.white_user_id
+          )
         expect(@white_queen.valid_move?(1, 3)).to eq false
       end
     end
