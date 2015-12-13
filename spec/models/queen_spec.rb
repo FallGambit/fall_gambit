4b7726813d570c44d025ebda9406b63d8cc088af
amarkpark <nospam@amarkpark.com>
2015-12-13 21:56:15 +0000

    working out kinks in test variables ~AMP

--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -6,18 +6,19 @@ RSpec.describe Queen, type: :model do
       @board = create(:game)
       @board.pieces.delete_all
       @board.reload
+      @white_user = User.find(@board.white_user_id)
+      @black_user = User.find(@black_user_id)
     end
-
-    context "no obstruction, no end piece, all pass: " do
+    context "no obstruction, no end piece, all pass:" do
       before :all do
         @white_queen = Queen.create(
           x_position: 3,
           y_position: 3,
           color: true,
           game_id: @board.id
+          user_id: @white_user.id
           )
       end
-
       it "diagonal +X +Y" do
         expect(@white_queen.valid_move?(6, 6)).to eq true
       end
@@ -43,19 +44,13 @@ RSpec.describe Queen, type: :model do
         expect(@white_queen.valid_move?(1, 3)).to eq  true
       end
     end
-
-    context "no obstruction, opposite color end piece, all pass: " do
+    context "no obstruction, opposite color end piece, all pass:" do
       before :all do
+        @white_queen.update(y_position: 0)
         @black_queen = Queen.create(
           x_position: 3,
           y_position: 2,
           color: false,
-          game_id: @board.id
-          )
-        @white_queen = Queen.create(
-          x_position: 3,
-          y_position: 0,
-          color: true,
           game_id: @board.id
           )
         @white_rook = Rook.create(
@@ -70,7 +65,6 @@ RSpec.describe Queen, type: :model do
           color: true,
           game_id: @board.id
           )
-
         @white_knight1 = Knight.create(
           x_position: 1,
           y_position: 4,
@@ -101,88 +95,151 @@ RSpec.describe Queen, type: :model do
           color: true,
           game_id: @board.id
           )
+        @
       end
-
       it "diagonal +X +Y" do
-
+        expect(@black_queen.valid_move?(5, 4)).to eq true
       end
       it "diagonal +X -Y" do
-
+        expect(@black_queen.valid_move?(5, 0)).to eq true
       end
       it "diagonal -X -Y" do
-
+        expect(@black_queen.valid_move?(1, 0)).to eq true
       end
       it "diagonal -X +Y" do
-
+        expect(@black_queen.valid_move?(1, 4)).to eq true
       end
       it "vertical +Y" do
-
+        expect(@black_queen.valid_move?(3, 4)).to eq true
       end
       it "vertical -Y" do
-
+        expect(@black_queen.valid_move?(3, 0)).to eq true
       end
       it "horizontal +X" do
-
+        expect(@black_queen.valid_move?(5, 2)).to eq true
       end
       it "horizontal -X" do
-
+        expect(@black_queen.valid_move?(1, 2)).to eq true
       end
     end
-
-    context "no obstruction, same color end piece, all fail: " do
+    context "no obstruction, same color end piece, all fail:" do
+      before :all do
+        byebug
+        @black_queen.update(x_position: 5, y_position: 5)
+        @white_queen.update(y_position: 2)
+        @white_king = King.create(
+          x_position: 3,
+          y_position: 0,
+          color: true,
+          game_id: @board.id
+          )
+      end
       it "diagonal +X +Y" do
-
+        expect(@white_queen.valid_move?(5, 4)).to eq false
       end
       it "diagonal +X -Y" do
-
+        expect(@white_queen.valid_move?(5, 0)).to eq false
       end
       it "diagonal -X -Y" do
-
+        expect(@white_queen.valid_move?(1, 0)).to eq false
       end
       it "diagonal -X +Y" do
-
+        expect(@white_queen.valid_move?(1, 4)).to eq false
       end
       it "vertical +Y" do
-
+        expect(@white_queen.valid_move?(3, 4)).to eq false
       end
       it "vertical -Y" do
-
+        expect(@white_queen.valid_move?(3, 0)).to eq false
       end
       it "horizontal +X" do
-
+        expect(@white_queen.valid_move?(5, 2)).to eq false
       end
       it "horizontal -X" do
-
+        expect(@white_queen.valid_move?(1, 2)).to eq false
       end
     end
-
-    context "with obstruction, all fail: " do
+    context "with obstruction, all fail:" do
+      before :all do
+        @black_pawn1 = Pawn.create(
+          x_position: 4,
+          y_position: 6,
+          color: false,
+          game_id: @board.id
+          )
+        @black_pawn2 = Pawn.create(
+          x_position: 5,
+          y_position: 6,
+          color: false,
+          game_id: @board.id
+          )
+        @black_pawn3 = Pawn.create(
+          x_position: 6,
+          y_position: 6,
+          color: false,
+          game_id: @board.id
+          )
+        @black_rook = Rook.create(
+          x_position: 6,
+          y_position: 5,
+          color: false,
+          game_id: @board.id
+          )
+        @black_bishop = Bishop.create(
+          x_position: 4,
+          y_position: 5,
+          color: false,
+          game_id: @board.id
+          )
+        @white_pawn3 = Pawn.create(
+          x_position: 4,
+          y_position: 4,
+          color: false,
+          game_id: @board.id
+          )
+        @white_pawn4 = Pawn.create(
+          x_position: 6,
+          y_position: 4,
+          color: false,
+          game_id: @board.id
+          )
+      end
       it "diagonal +X +Y" do
-
+        expect(@black_queen.valid_move?(7, 7)).to eq false
       end
       it "diagonal +X -Y" do
-
+        expect(@black_queen.valid_move?(7, 3)).to eq false
       end
       it "diagonal -X -Y" do
-
+        expect(@black_queen.valid_move?(3, 3)).to eq false
       end
       it "diagonal -X +Y" do
-
+        expect(@black_queen.valid_move?(3, 7)).to eq false
       end
       it "vertical +Y" do
-
+        expect(@black_queen.valid_move?(5, 7)).to eq false
       end
       it "vertical -Y" do
-
+        expect(@black_queen.valid_move?(5, 3)).to eq false
       end
       it "horizontal +X" do
-
+        expect(@black_queen.valid_move?(7, 5)).to eq false
       end
       it "horizontal -X" do
-
+        expect(@black_queen.valid_move?(3, 5)).to eq false
       end
     end
-
-    context "invalid coordinates, all fail: "
+    context "invalid coordinates, all fail:" do
+      it "tries to move off the board" do
+        expect(@black_queen.valid_move?(8, 2)).to eq false
+      end
+      it "tries to make invalid move" do
+        expect(@white_queen.valid_move?(1, 3)).to eq false
+      end
+    end
+    after :all do
+      @board.pieces.delete_all
+      @board.delete
+    end
   end
 end
